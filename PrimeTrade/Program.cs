e9016e596dd50c3e69a931aad7f07dd7627af898
Delushaan <delushaan@outlook.com>
2019-02-21 14:30:00 +0100

    window

--- PrimeTrade/Program.cs
+++ PrimeTrade/Program.cs
@@ -16,7 +16,7 @@ namespace PrimeTrade
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmManagerStockManagement());
+            Application.Run(new frmStartup());
         }
     }
 }
