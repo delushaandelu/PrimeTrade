ca42dcdc4039ee1ab5e0b8469723403a18494d82
Delushaan <delushaan@outlook.com>
2019-05-25 17:00:12 +0530

    report changes

---
+++ PrimeTrade/frmReportViewer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework;
+using MetroFramework.Fonts;
+using MetroFramework.Forms;
+using MySql.Data.MySqlClient;
+using PrimeTrade.connections;
+using Syncfusion;
+
+
+namespace PrimeTrade
+{
+    public partial class frmReportViewer : MetroForm
+    {
+        public frmReportViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void frmReportViewer_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
