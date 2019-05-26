a1da974bccd99bf97ce6c3defe6779a1813b3af2
Mohan Delushaan <mohan.delushaan@ifsworld.com>
2019-05-26 15:42:06 +0530

    migration end

--- PrimeTrade/Program.cs
+++ PrimeTrade/Program.cs
@@ -16,7 +16,7 @@ namespace PrimeTrade
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmManagerGenerateReport());
+            Application.Run(new frmStartup());
         }
     }
 }
